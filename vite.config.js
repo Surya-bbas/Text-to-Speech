331ccf161bb5d8f07f0f093956c4a29af4ff5326
Shabari02 <lkshabarinathan02@gmail.com>
2023-01-07 20:09:11 +0530

    Modified

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base:'/Text-to-Speech/',
   plugins: [react()],
+  mode: "development",
+  build: {
+    minify: false,
+  }
 })
