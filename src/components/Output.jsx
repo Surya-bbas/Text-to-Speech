d1a21520d18b01c84591a4b1900c645819f3635a
Shabari02 <lkshabarinathan02@gmail.com>
2023-01-07 12:18:32 +0530

    modified to normal

--- src/components/Output.jsx
+++ src/components/Output.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 
 const Output = ({data}) => {
 
-    
+    const apikey = "a51eb574f38944c28cf78061597b49ba";
+    const AudioSrc = data;
+    const mylink = "https://api.voicerss.org/?key="+apikey+"&hl=en-us&src=" + AudioSrc;
 
     return (
         <div>
